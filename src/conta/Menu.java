a1694d40d7203445aecb20e20fdb8c61fdae87bd
WilliamReisO <oliveira._r@hotmail.com>
2023-03-28 12:30:30 -0300

    Criando interface

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -9,16 +9,7 @@ public class Menu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				
-				// Teste da Classe 
-		
-				Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-				c1.visualizar();
-				c1.sacar(12000.0f);
-				c1.visualizar();
-				c1.depositar(5000.0f);
-				c1.visualizar ();
 			
-				
 		        
 				// Teste da Classe Conta Corrente
 				ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
